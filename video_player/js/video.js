4cc06d5bcff725acbea61d44b9eca2bc72bd728f
Meghan Tucker <meghanctucker@gmail.com>
2020-04-17 15:06:13 -0400

    Main Styling Update
    Updated the CSS to format correctly, added comments in JS file to begin
    writing program.

---
+++ video_player/js/video.js
@@ -0,0 +1,19 @@
+//const spanItems = document.getElementsByTagName("span")[0];
+
+//spanItems.addEventListener("click", () => {
+//    spanItems.textContent = spanItems.textContent.toUpperCase();
+//});
+// use data-start and data-end attributes to span each target
+//create an event listener
+//trigger as video time updates
+//inside listener loop
+  //over each listener
+    //inside loop use conditional
+      //video current time > data-attribute-start && <data-attribute-end
+    //IF conditions == true
+      //set <span> {color: #f7ac57;}
+    //else <span> {color: original color;}
+
+
+//EXTRA CREDIT:
+  //IF <Span> is clicked, jump video to that place.
