3b94a9da538666fc9a3e3e27bda4cebb013fb734
Meghan Tucker <meghanctucker@gmail.com>
2020-04-21 14:45:01 -0400

    Meets expectations
    for and while loops are running, meets expectations of project.

--- video_player/js/video.js
+++ video_player/js/video.js
@@ -1,19 +1,51 @@
 
-//create array to pull data start attribute value and set it as new array value. 
+//create array to pull data start attribute value and set it as new array value.
 
 //const spanItems = document.getElementsByTagName("span")[0];
 //const testing = document.getElementsByClassName("testClick");
 const dStart = document.querySelectorAll("span[data-start]");
+const video = document.querySelector("video");
 
+
+//const startTime = dStart[0].dataset.start;
 //spanItems.addEventListener("click", () => {
 //    spanItems.textContent = spanItems.textContent.toUpperCase();
 //});
 
-  let myValue = dStart.getAttribute("data-start"); //This should get the data start value
-  console.log(myValue);
+//  let myValue = dStart.getAttribute("data-start"); //This should get the data start value
+//  console.log(myValue);
 
 // use data-start and data-end attributes to span each target
+video.addEventListener("timeupdate", () => {
+//  console.log(video.currentTime);
+  for (let i = 0; dStart.length; i += 1) {
+    let myDStart = dStart[i];
+    let startTime = dStart[i].dataset.start;
+    let endTime = dStart[i].dataset.end;
+    let playTime = video.currentTime;
+  //  if startTime >= currenttime && endTime <= currenttime { myDStart.style.color = "#f7ac57;"}
+  // else if (false) { myDStart.style.color = "black";}
 
+//  }
+        if (startTime <= playTime && endTime >= playTime) {
+          myDStart.style.color = "#f7ac57";
+        }
+        else {
+          myDStart.style.color = "black";
+        }
+
+
+      //    myDStart.addEventListener("mouseover", () => {
+//          myDStart.style.color = "";
+  //      });
+//
+  //        myDStart.addEventListener("mouseout", () => {
+    //      myDStart.style.color = "";
+      //  });
+  //console.log(startTime);
+  //console.log(endTime);
+};
+});
 //create an event listener
 //trigger as video time updates
 //inside listener loop
@@ -23,15 +55,14 @@ const dStart = document.querySelectorAll("span[data-start]");
     //IF conditions == true
       //set <span> {color: #f7ac57;}
     //else <span> {color: original color;}
-    for (let i = 0; dStart.length; i += 1) {
-    dStart[i].addEventListener("mouseover", () => {
-      dStart[i].style.color = "#f7ac57";
-    });
 
-    dStart[i].addEventListener("mouseout", () => {
-      dStart[i].style.color = "black";
-    })
-    }
+
+  //  for (let i = 0; dstart.length; i += 1) {
+dStart.addEventListener( 'click', () => {
+  //get the current span
+  //set video playback time to data-start
+})
+
 
 
 //EXTRA CREDIT:
