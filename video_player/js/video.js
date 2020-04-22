ec8992f98153611548ce3569e76f141248eb8550
Meghan Tucker <meghanctucker@gmail.com>
2020-04-22 15:05:59 -0400

    Clean up Meets expectations
    Cleaned up comments and made code readable and with comments, adjusted
    data-end attribute in the html to reflect timer.

--- video_player/js/video.js
+++ video_player/js/video.js
@@ -1,69 +1,27 @@
 
-//create array to pull data start attribute value and set it as new array value.
-
-//const spanItems = document.getElementsByTagName("span")[0];
-//const testing = document.getElementsByClassName("testClick");
+//create variables targeting the transcript spans and video
 const dStart = document.querySelectorAll("span[data-start]");
 const video = document.querySelector("video");
 
-
-//const startTime = dStart[0].dataset.start;
-//spanItems.addEventListener("click", () => {
-//    spanItems.textContent = spanItems.textContent.toUpperCase();
-//});
-
-//  let myValue = dStart.getAttribute("data-start"); //This should get the data start value
-//  console.log(myValue);
-
-// use data-start and data-end attributes to span each target
+//create an event listener
+//trigger as video time updates
 video.addEventListener("timeupdate", () => {
-//  console.log(video.currentTime);
+  //inside listener loop
   for (let i = 0; dStart.length; i += 1) {
+    //over each listener
     let myDStart = dStart[i];
-    let startTime = dStart[i].dataset.start;
+    let startTime = dStart[i].getAttribute("data-start");
     let endTime = dStart[i].dataset.end;
     let playTime = video.currentTime;
-  //  if startTime >= currenttime && endTime <= currenttime { myDStart.style.color = "#f7ac57;"}
-  // else if (false) { myDStart.style.color = "black";}
-
-//  }
-        if (startTime <= playTime && endTime >= playTime) {
+        //inside loop use conditional
+        //IF conditions == true
+        if (startTime <= playTime && playTime <= endTime) {
+          //set <span> {color: #f7ac57;}
           myDStart.style.color = "#f7ac57";
         }
+        //else <span> {color: original color;}
         else {
           myDStart.style.color = "black";
         }
-
-
-      //    myDStart.addEventListener("mouseover", () => {
-//          myDStart.style.color = "";
-  //      });
-//
-  //        myDStart.addEventListener("mouseout", () => {
-    //      myDStart.style.color = "";
-      //  });
-  //console.log(startTime);
-  //console.log(endTime);
-};
+      };
 });
-//create an event listener
-//trigger as video time updates
-//inside listener loop
-  //over each listener
-    //inside loop use conditional
-      //video current time > data-attribute-start && <data-attribute-end
-    //IF conditions == true
-      //set <span> {color: #f7ac57;}
-    //else <span> {color: original color;}
-
-
-  //  for (let i = 0; dstart.length; i += 1) {
-dStart.addEventListener( 'click', () => {
-  //get the current span
-  //set video playback time to data-start
-})
-
-
-
-//EXTRA CREDIT:
-  //IF <Span> is clicked, jump video to that place.
